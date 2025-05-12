ed19554d672c2efa45b6013d1aede6c4c7757aad
Matheusbarros54 <matheus.barros@sptech.school>
2025-05-11 23:19:00 -0300

    feat: class library application

---
+++ MathOperations.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleMathLibrary
+{
+    public class MathOperations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ tests/MathOperationTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleMathLibrary;
+
+namespace SimpleMathLibrary.Tests
+{
+    [TestClass]
+    public class MathOperationsTests
+    {
+        [TestMethod]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            // Arrange
+            var math = new MathOperations();
+            int a = 5, b = 3;
+
+            // Act
+            int result = math.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void Subtract_ShouldReturnCorrectDifference()
+        {
+            // Arrange
+            var math = new MathOperations();
+            int a = 5, b = 3;
+
+            // Act
+            int result = math.Subtract(a, b);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+    }
+}
